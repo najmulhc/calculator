953b62f1c672bfd8bc310f3e19a3a63dd7f42d98
kridi32 <najmulhudachowdhury@gmail.com>
2022-02-12 18:21:44 +0600

    final

---
+++ app.js
@@ -0,0 +1,34 @@
+
+let equation = "";
+let keys =  document.getElementsByClassName("key");
+for(let key of keys){
+    key.addEventListener("click", function(){
+      let keypressed = key.innerText;
+      console.log(keypressed);
+      if(keypressed == "="){
+        resultUpdate(equation);
+        equation = "";
+        equationUpdate(equation);
+      }
+      else if(keypressed == ""){
+          equation = equation.slice(0,-1);
+          equationUpdate(equation);
+      }
+      else if(keypressed == "C"){
+        document.getElementById("display").innerText = "";
+        equation = "";
+        equationUpdate(equation);
+      }
+      else{
+          equation += keypressed;
+          equationUpdate(equation);
+      }
+    });
+}
+function equationUpdate(e){
+    document.getElementById("equationDisplay").innerText = e; 
+}
+function resultUpdate(e){
+    let result  = eval(e);
+    document.getElementById("display").innerText = result;
+}
